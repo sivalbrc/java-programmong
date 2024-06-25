a143fe4d52587514102be315d0b117bddf681383
vedurusivaprasadreddy <152365210+sivalbrc@users.noreply.github.com>
2024-06-25 20:43:06 +0530

    Add files via upload

---
+++ OperatorsTest.java
@@ -0,0 +1,11 @@
+package com.traningmug.java;
+
+public class OperatorsTest {
+    public static void main(String[] args) {
+        int salary;
+        salary=300000;
+        int incrementedSalary;
+        incrementedSalary = salary+(salary*10)/100;
+        System.out.println("IncrementedSalary : "+ incrementedSalary);
+    }
+}
